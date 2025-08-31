13db46e631f3b335985e04da73220da8e613120e
yaserfarook1 <98206606+yaserfarook1@users.noreply.github.com>
2025-08-31 17:39:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-import uvicorn
 
 app = FastAPI()
 
 @app.get("/hello")
 def hello():
     return JSONResponse(content={"message": "Hello Farook, what do you want?"})
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8501)
